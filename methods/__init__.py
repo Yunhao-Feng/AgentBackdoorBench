9f943d0092610ec418cded0fac3657eb2cefdf18
YunHao-Von <Alctrain@foxmail.com>
2025-07-15 15:59:11 +0800

    0715 Transfer from maslab

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,18 +1,46 @@
 from .mas_base import MAS
 from .cot import CoT
-from .autogen import AutoGen_Main
 from .agentverse import AgentVerse_HumanEval, AgentVerse_MGSM, AgentVerse_Main
+from .llm_debate import LLM_Debate_Main
+from .dylan import DyLAN_HumanEval, DyLAN_MATH, DyLAN_MMLU, DyLAN_Main
+from .autogen import AutoGen_Main
+from .camel import CAMEL_Main
+from .evomac import EvoMAC_Main
 from .chatdev import ChatDev_SRDD
+from .macnet import MacNet_Main, MacNet_SRDD
+from .mad import MAD_Main
+from .mapcoder import MapCoder_HumanEval, MapCoder_MBPP
+from .self_consistency import SelfConsistency
+from .mav import MAV_GPQA, MAV_HumanEval, MAV_Main, MAV_MATH, MAV_MMLU
+
 
 
 method2class = {
     "vanilla": MAS,
     "cot": CoT,
-    "autogen": AutoGen_Main,
-    "agentverse":AgentVerse_Main,
+    "agentverse_humaneval": AgentVerse_HumanEval,
     "agentverse_mgsm": AgentVerse_MGSM,
     "agentverse": AgentVerse_Main,
+    "llm_debate": LLM_Debate_Main,
+    "dylan_humaneval": DyLAN_HumanEval,
+    "dylan_math": DyLAN_MATH,
+    "dylan_mmlu": DyLAN_MMLU,
+    "dylan": DyLAN_Main,
+    "autogen": AutoGen_Main,
+    "camel": CAMEL_Main,
+    "evomac": EvoMAC_Main,
     "chatdev_srdd": ChatDev_SRDD,
+    "macnet": MacNet_Main,
+    "macnet_srdd": MacNet_SRDD,
+    "mad": MAD_Main,
+    "mapcoder_humaneval": MapCoder_HumanEval,
+    "mapcoder_mbpp": MapCoder_MBPP,
+    "self_consistency": SelfConsistency,
+    "mav_gpqa": MAV_GPQA,
+    "mav_humaneval": MAV_HumanEval,
+    "mav_main": MAV_Main,
+    "mav_math": MAV_MATH,
+    "mav_mmlu": MAV_MMLU
 }
 
 def get_method_class(method_name, dataset_name=None):
